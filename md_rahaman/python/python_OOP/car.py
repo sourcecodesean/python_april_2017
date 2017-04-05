a9e73917ddc9136fe68d3dc4adc61f7ed9526768
Md Rahaman <soikatesc@gmail.com>
2017-04-05 01:49:17 -0500

    This is modified and latest OOp car assignment

--- md_rahaman/python/python_OOP/car.py
+++ md_rahaman/python/python_OOP/car.py
@@ -22,8 +22,8 @@ class Car(object):
 			print "Tax = ",self.tax
 
 car1 = Car(2000,"35mph","Full","15mpg")
-car1 = Car(2000,"5mph","Not Full","105mpg")
-car1 = Car(2000,"15mph","Kind of Full","95mpg")
-car1 = Car(2000,"25mph","Full","25mpg")
-car1 = Car(2000,"45mph","Empty","25mpg")
-car1 = Car(2000000,"35mph","Empty","25mpg")
+car2 = Car(2000,"5mph","Not Full","105mpg")
+car3 = Car(2000,"15mph","Kind of Full","95mpg")
+car4 = Car(2000,"25mph","Full","25mpg")
+car5 = Car(2000,"45mph","Empty","25mpg")
+car6 = Car(2000000,"35mph","Empty","25mpg")
